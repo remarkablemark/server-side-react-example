2286c860ae766d77ce082f20f691f00981437f09
Mark <mark@remarkablemark.org>
2016-05-14 22:16:24 -0400

    Update static directory used by server
    - Rename 'static' to 'src', which now builds to 'build' with
      webpack
    - Update `npm start` command to run webpack before the server
    - Tidy up 'server.js' and update with new public path for static
      files

--- server.js
+++ server.js
@@ -8,21 +8,25 @@ const url = require('url');
 const fs = require('fs');
 const path = require('path');
 
+const PUBLIC_PATH = 'build';
+const PORT = 3000;
+const IP = '127.0.0.1';
+
 /**
- * Server.
+ * Create web server.
  */
 const server = http.createServer((req, res) => {
     console.log(`${req.method} ${req.url}`);
 
     // static file
-    if (req.method === 'GET' && req.url.indexOf('/static/') === 0) {
+    if (req.method === 'GET' && req.url.indexOf(`/${PUBLIC_PATH}/`) === 0) {
         const pathname = url.parse(req.url).pathname;
         const filepath = path.join(__dirname, pathname);
 
         fs.lstat(filepath, (err, stats) => {
             if (err) {
-                res.writeHead(500);
-                return res.end('Internal Server Error');
+                res.writeHead(404);
+                return res.end('Not Found');
             }
 
             if (stats.isFile()) {
@@ -53,16 +57,16 @@ const server = http.createServer((req, res) => {
                 </head>
                 <body>
                     <h1>Hello world!</h1>
-                    <script src='/static/main.js'></script>
+                    <script src='/${PUBLIC_PATH}/bundle.js'></script>
                 </body>
             </html>
         `);
     }
 });
 
-const PORT = 3000;
-const IP = '127.0.0.1';
-
+/**
+ * Listen to web server.
+ */
 server.listen(PORT, IP, () => {
     console.log(`http://${IP}:${PORT}`);
 });
