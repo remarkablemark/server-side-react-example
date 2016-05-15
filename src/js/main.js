d1cd01ecb5e661a9b8ba56e183d6aabe1c1f29a5
Mark <mark@remarkablemark.org>
2016-05-15 12:25:37 -0400

    Wait for `DOMContentLoaded` before React app is mounted client-side
    - `DOMContentLoaded` event will wait until whole document (HTML)
      has been loaded
    - This is to ensure the JSON props are loaded first before the
      script (otherwise an 'Invariant Violation' will occur due to
      universal/isomorphic setup)
    - Now the script can be loaded anywhere on the page (head, body)

--- src/js/main.js
+++ src/js/main.js
@@ -9,10 +9,12 @@ const ReactDOM = require('react-dom');
 /**
  * Render app on the client-side.
  */
-ReactDOM.render(
-    React.createElement(
-        require('../../components/App.jsx'),
-        window.__PROPS__
-    ),
-    document
-);
+document.addEventListener('DOMContentLoaded', function() {
+    ReactDOM.render(
+        React.createElement(
+            require('../../components/App.jsx'),
+            window.__PROPS__
+        ),
+        document
+    );
+});
